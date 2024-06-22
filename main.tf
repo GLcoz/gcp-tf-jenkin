af8aa28fae7798cdbe246a9e0157a09fbd8784b4
hakim fatmi <126871854+GLcoz@users.noreply.github.com>
2024-06-22 01:38:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "tt-githubdemo-bucket-001"
   project                  = "devops-project-427122"
-  location                 = "US"
+  location                 = "AFRICA"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
@@ -9,7 +9,7 @@ resource "google_storage_bucket" "my-bucket" {
 resource "google_storage_bucket" "my-bucket2" {
   name                     = "tt-githubdemo-bucket-002"
   project                  = "devops-project-427122"
-  location                 = "US"
+  location                 = "AFRICA"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
